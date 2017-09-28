c113ad0315098a13c4b2448de44e9369202be7a2
Mat Malone <mmalone@datacare.com>
2017-09-28 15:51:26 -0700

    up to functional components

--- src/index.js
+++ src/index.js
@@ -1,20 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import $ from "jquery";
 
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square 
+        value={this.state.squares[i]} 
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
@@ -65,3 +84,8 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+$(() => {
+    console.log('ready');
+    // $('.square').click(() => console.log('clicky'));
+});
