5b5e49c4b6528337fa00126aff8bf6c90acfa04e
Mat Malone <mmalone@datacare.com>
2017-09-28 18:11:58 -0700

    jumpTo

--- src/index.js
+++ src/index.js
@@ -51,12 +51,21 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null),
       }],
+      stepNumber: 0,
       xIsNext: true,
     };
   }
 
+  jumpTo(step)
+  {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    });
+  }
+
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
 
@@ -71,14 +80,14 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares,
       }]),
-      squares: squares,
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
@@ -86,7 +95,7 @@ class Game extends React.Component {
         'Move #' + move :
         'Game start';
       return (
-        <li>
+        <li key={move}>
           <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
         </li>
       );
